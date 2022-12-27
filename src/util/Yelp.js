1a874c168c8b026e799b3919af4cb1b6588f7b98
Jeffery Jiang <jeffery.jiang1992@gmail.com>
2022-12-27 15:22:16 +1100

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,35 @@
+import SearchBar from "../components/SearchBar/SearchBar"
+
+const apiKey = "vpuIJObJMVUDYPsBcXlejjfVolwUM6Dtgg2mvCyNbUbCGT1_CygOFHRtezJuOqAsu4BTdSHpVV5oaiNcsriKoS5ui0aQe8BkBmofLCW6faYcEIwaxpVQp3RAc0aVY3Yx"
+const Yelp = {
+    search(term, location, sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: { 
+                Authorization: `Bearer ${apiKey}`
+            }
+        }
+    )
+    .then(response => response.json())
+    .then(jsonResponse => {
+        if (jsonResponse.businesses){
+            return jsonResponse.businesses.map(business => {
+               //console.log(business)
+                return  {
+                id: business.id,
+                imageSrc: business.image_url,
+                name: business.name,
+                address: business.location.address1,
+                city: business.location.city,
+                state: business.location.zip_code,
+                zipCode: business.location.zip_code,
+                category: business.categories[0].title,
+                rating: business.rating,
+                reviewCount: business.review_count }
+            })
+        }
+    })
+}}
+
+export default Yelp
+
